7b55256815854f85e318ba379dd0390238eaf717
marina <marina.kukso@gmail.com>
2022-02-05 17:40:09 -1000

    rougier: dashes ok

--- rougier.js
+++ rougier.js
@@ -1,6 +1,6 @@
 var regl = require('regl')( { extensions: ['angle_instanced_arrays'] })
 var vec2 = require('gl-vec2')
-var lineData = [-0.3,-0.1, -0.2,0.4, 0.3,-0.9, 0.35,0.4, 0.6,-0.2]
+var lineData = [-0.3,-0.1, -0.2,0.4, 0.3,-0.9, 0.35,0.5, 0.6,-0.2]
 var v0 = [0, 0]
 var v1 = [0, 0]
 var nv = [0, 0]
@@ -44,11 +44,14 @@ function addRect (rect, a, b, width) {
   var lab = vec2.distance(a, b)
   rect.lengths.push(lab, lab, lab, lab)
   rect.uvs.push(-width, width, -width, -width, lab+width, -width, lab+width, width)
-  var ph = 0
+  var ph = width
   if (rect.phases.length > 0) {
     var l = rect.lengths[rect.lengths.length-1]
     var p = rect.phases[rect.phases.length-1]
-    ph = l + p
+    ph = l + p - width*3
+    rect.prevPhases.push(p, p, p, p)
+  } else {
+    rect.prevPhases.push(0, 0, 0, 0)
   }
   rect.phases.push(ph, ph, ph, ph)
 }
@@ -59,8 +62,9 @@ var line = {
   uvs: [],
   lengths: [],
   phases: [],
-  period: 5,
-  width: 0.1
+  prevPhases: [],
+  period: 7,
+  width: 0.005
 }
 
 var a = [0, 0]
@@ -75,32 +79,42 @@ for (var i=0; i<lineData.length/2-1; i++) {
 var draw = regl({
   frag: `
     precision highp float;
-    varying float vlab, vphase;
+    varying float vlab, vphase, vPrevPhase;
     varying vec2 vuv;
     uniform float width, period;
     void main() {
       float freq = width * period;
-      float uu = mod((vuv.x + vphase)/freq, 1.0);
       float scap = step(vuv.x, 0.0);
       float ecap = step(vlab, vuv.x);
       float bcap = (1.0-scap)*(1.0-ecap);
       if (scap > 0.5 && length(vuv) > width) discard;
       vec2 buv = vec2(vlab,0);
       if (ecap > 0.5 && distance(vuv,buv) > width) discard;
+      float uu = mod((clamp(0.0, vlab, vuv.x) + vphase)/freq, 1.0);
+      /*
+      float uu = 0.0;
+      if (vuv.x < 0.0) {
+        uu = mod((vphase+width)/freq, 1.0);
+      } else {
+        uu = mod((clamp(0.0, vlab, vuv.x) + vphase)/freq, 1.0);
+      }
+      */
       float x = step(0.5, uu);
+      if (x > 0.5) discard;
       gl_FragColor = vec4(x, 0.0, 1.0, 1.0);
       //gl_FragColor = vec4(scap, ecap, uu, 1);
     }`,
   vert: `
     precision highp float;
-    attribute float lab, phase;
+    attribute float lab, phase, prevPhase;
     attribute vec2 position, uv;
-    varying float vlab, vphase;
+    varying float vlab, vphase, vPrevPhase;
     varying vec2 vuv;
     void main() {
       vlab = lab;
       vuv = uv;
       vphase = phase;
+      vPrevPhase = prevPhase;
       gl_Position = vec4(position, 0, 1);
     }`,
   uniforms: {
@@ -112,6 +126,7 @@ var draw = regl({
     uv: line.uvs,
     lab: line.lengths,
     phase: line.phases,
+    prevPhase: line.prevPhases,
   },
   elements: line.cells,
   primitive: "triangles",
